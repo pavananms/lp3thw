bd36c94094ecad74a7b666e9b2af1debdb16388b
pavananms <pavananms@gmail.com>
2019-02-07 15:34:21 +0530

    excercises 4 to 16

--- ex4.py
+++ ex4.py
@@ -5,4 +5,13 @@ passengers = 90
 cars_not_driven = cars - drivers
 cars_driven = drivers
 carpool_capacity = cars_driven + space_in_a_car
-average_passengers
+average_passengers_per = passengers / cars_driven
+
+print("There are",cars,"available.")
+print("There are only", drivers, "drivers available.")
+print("There will be", cars_not_driven, "empty cars today.")
+print("We can transport", carpool_capacity, "people today.")
+print("We have", passengers, "to carpool today.")
+print("We need to put about",average_passengers_per_car,"in each car.")
+
+
---
+++ ex5.py
@@ -0,0 +1,19 @@
+
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs 
+eyes = 'blue' 
+teeth = 'white'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+
+
+
+
+
